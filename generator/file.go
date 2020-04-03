9b8cca752450a9a4e20e5aa4edf652b6dc407997
Alihan Doğuş Yalçın <dogus544@gmail.com>
2020-04-03 21:15:22 +0300

    removed unnecessary lines

--- generator/file.go
+++ generator/file.go
@@ -18,87 +18,18 @@ func checkError(e error) {
 func (p *project) create() {
 	fmt.Println("Starting to generate project", p.name)
 
-	p.cre("internal", "pkg")
-	p.cre("internal", "service")
-	p.cre("cmd", "service")
-	// Create project directory
-	/*err := os.Mkdir(p.name, 0755)
-	checkError(err)
+	p.walk("internal", "/pkg")
+	p.walk("internal", "/service")
+	p.walk("cmd", "/service")
+	p.createFilesAndDirectories("README.md", basePath+"README.md", "", "")
+	p.createFilesAndDirectories("go.mod", basePath+"go.mod", "", "")
+	p.createFilesAndDirectories("VERSION", basePath+"VERSION", "", "")
+	p.createFilesAndDirectories("version.go", basePath+"version.go", "", "")
 
-	// create directories
-	for _, path := range directories {
-		err := os.MkdirAll(p.name+path, 0775)
-		checkError(err)
-	}
-	fmt.Println("Directories created.")
-
-	for _, f := range files {
-		sfile, err := ioutil.ReadFile(basePath + f)
-		checkError(err)
-
-		dfile := strings.Replace(string(sfile), "project", p.name, -1)
-		err = ioutil.WriteFile(p.name+f, []byte(dfile), 0644)
-		checkError(err)
-		fmt.Println(p.name + f + " created.")
-	}
-
-	// create /internal/services*
-	p.createInternalServices()
-
-	// create /cmd/services*
-	p.createCmdServices()
-
-	fmt.Println("micgo completed.")*/
+	fmt.Println("micgo completed.")
 }
 
-func (p *project) createInternalServices() {
-	for service, _ := range p.services {
-		// create directories
-		servicePath := p.name + internal + "/" + service
-		fullServicePath := servicePath + "/config"
-		err := os.MkdirAll(fullServicePath, 0775)
-		checkError(err)
-
-		err = filepath.Walk(basePath+internalService,
-			func(path string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-				if strings.Contains(path, ".go") {
-					name := strings.Split(path, "/")
-					// get index of "service"
-					index := func() int {
-						for k, v := range name {
-							if v == "service" {
-								return k
-							}
-						}
-						return 0
-					}()
-
-					fileName := strings.Join(name[index+1:], "/")
-					// create config.go files
-					sfile, err := ioutil.ReadFile(path)
-					if err != nil {
-						return err
-					}
-
-					replacer := strings.NewReplacer("project", p.name, "servicename", service)
-					dfile := replacer.Replace(string(sfile))
-
-					err = ioutil.WriteFile(servicePath+"/"+fileName, []byte(dfile), 0644)
-					if err != nil {
-						return nil
-					}
-					fmt.Println(servicePath + "/" + fileName + " created.")
-				}
-				return nil
-			})
-		checkError(err)
-	}
-}
-
-func (p *project) cre(key, dir string) {
+func (p *project) walk(key, dir string) {
 	service := make(map[string]int)
 	if strings.Contains(dir, "service") {
 		service = p.services
@@ -111,7 +42,7 @@ func (p *project) cre(key, dir string) {
 			err := os.MkdirAll(p.name+"/"+key+"/"+service, 0775)
 			checkError(err)
 		}
-		err := filepath.Walk(basePath+"/"+key+"/"+dir,
+		err := filepath.Walk(basePath+key+dir,
 			func(path string, info os.FileInfo, err error) error {
 				if err != nil {
 					return err
@@ -142,17 +73,20 @@ func (p *project) cre(key, dir string) {
 	}
 }
 
-func (p *project) createFilesAndDirectories(fileName, path, service string, port string) error {
+func (p *project) createFilesAndDirectories(fileName, path, service, port string) error {
 	if strings.Contains(fileName, ".go") ||
 		strings.Contains(fileName, "Dockerfile") ||
-		strings.Contains(fileName, ".toml") {
+		strings.Contains(fileName, ".toml") ||
+		strings.Contains(fileName, ".md") ||
+		strings.Contains(fileName, ".mod") ||
+		strings.Contains(fileName, "VERSION") {
 		source, err := ioutil.ReadFile(path)
 		if err != nil {
 			return err
 		}
 		replacer := strings.NewReplacer("project", p.name,
 			"servicename", service,
-			"portnumber", string(port))
+			"portnumber", port)
 
 		destination := replacer.Replace(string(source))
 
