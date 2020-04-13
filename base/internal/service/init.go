74d7335be996653b38862864ff088bd857037e1b
Alihan Doğuş Yalçın <dogus544@gmail.com>
2020-04-13 22:56:20 +0300

    DbClient added to services

--- base/internal/service/init.go
+++ base/internal/service/init.go
@@ -6,15 +6,19 @@ import (
 	"{project}/internal/pkg/bootstrap/startup"
 	"{project}/internal/pkg/di"
 	"{project}/internal/pkg/logger"
+	"{project}/internal/pkg/db/interfaces"
 	"sync"
 )
 
 var LoggingClient logger.LoggingClient
+var DbClient interfaces.DBClient
 
 func BootstrapHandler(wg *sync.WaitGroup, ctx context.Context, startupTimer startup.Timer, dic *di.Container) bool {
 
 	LoggingClient = container.LoggingClientFrom(dic.Get)
 	LoggingClient.Info("{servicename} microservice is initializing.")
+
+	DbClient = container.DBClientFrom(dic.Get)
 
 	return true
 }
